02f1e4b1a41d36e3ac48c82a8cfd783d5d054d59
kristynakorinkovanovainformatika <kristyna.korinkova@novainformatika.cz>
2026-01-09 14:58:46 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    basic.clearScreen()
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("hynek a mara")
     basic.showIcon(IconNames.Heart)
-    basic.showString("LOVE")
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    basic.clearScreen()
 })
